a33ef29c3e19a89041af94abeeb030fe70d95bf4
mediapills <mediapills@gmail.com>
2021-10-06 00:13:21 +0300

    Init

--- tests/unit/test_requests.py
+++ tests/unit/test_requests.py
@@ -18,3 +18,11 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+import unittest
+
+
+class TestRequestsImport(unittest.TestCase):
+    def test_import_should_not_fail(self) -> None:
+        __import__("mediapills.http_foundation.requests")
+
+        self.assertTrue(True)
